8090e57618e9e2672424a82e6925f0419509107c
Ryan Lowe <somebody480@gmail.com>
2022-08-13 09:10:31 -0400

    add event bubbling, comment code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+// DOM elements
 const inputsElement = document.querySelector("#input-list");
 const currentGoals = document.querySelector(".current-goals");
+const totalCountSpan = document.querySelector(".total-count");
+const completedCountSpan = document.querySelector(".completed-count");
+const percentSpan = document.querySelector(".percent");
 
+// root element for event bubbling on dynamically added elements
+const rootElement = document.querySelector("section");
+rootElement.addEventListener("click", (e) => {
+  markComplete(e.target);
+});
+
+// vars
 let completion = "none";
+let completeCount = 0;
+let totalCount = 0;
 const inputsArr = [];
 
+// input handler
 inputsElement.addEventListener("keyup", (e) => {
   if (e.key === "Enter" || e.keyCode === 13) {
     const inputsStr = inputsElement.value;
@@ -13,11 +27,26 @@ inputsElement.addEventListener("keyup", (e) => {
       currentGoals.innerHTML += input;
     });
     inputsElement.value = "";
+    totalCount += inputsArr.length;
   }
 });
 
+// helper function to parse input strings into DOM elements
 function parseStr(str) {
   str = str.trim();
-  strArr = str.split(",").map((substr) => `<li>${substr.trim()}</li>`);
+  strArr = str
+    .split(",")
+    .map((substr) => `<li id="item">${substr.trim()}</li>`);
   return strArr;
+}
+
+// helper function to mark items complete or incomplete
+function markComplete(element) {
+  if (!element.classList.contains("complete")) {
+    completeCount++;
+  } else {
+    completeCount--;
+  }
+  element.classList.toggle("complete");
+  completedCountSpan.innerHTML = completeCount;
 }
